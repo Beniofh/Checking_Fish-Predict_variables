850430b699d77a244b9ff0b8bd94d1a20a777066
Maximilien Servajean <servajean@gpu4.lirmm.fr>
2023-02-21 12:20:37 +0100

    pylintrc - for VSC and random forest 8% accuracy

---
+++ src/main_2102.py
@@ -0,0 +1,56 @@
+# %% import libraries
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+# %% loading dataframe
+df = pd.read_csv('../data/dataset.csv', sep=',', low_memory=False)
+
+
+
+# %% selecting feature
+quantitative_col = [
+    'bathymetry_band_0_mean_9x9',
+    'bathymetry_band_0_sd',
+    'chlorophyll_concentration_1km_band_0_mean_15x15',
+    'chlorophyll_concentration_1km_band_0_sd',
+    'salinity_4.2km_mean_year_band_0_mean_7x7',
+    'salinity_4.2km_mean_year_band_0_sd'
+    ]
+
+qualitative_col = [
+    'ecoregion'
+]
+for col in quantitative_col:
+    df = df[df[col] != 'occurence_out_time']
+df.reset_index(inplace=True)
+
+# %% processing labels
+le = LabelEncoder()
+le.fit(df.species.unique())
+df['labels'] = le.transform(df.species).astype(int)
+
+# %% Processing NaN on selected features
+for col in quantitative_col:
+    df.loc[df[col].isna(), col] = df[~df[col].isna()][col].astype(float).mean()
+
+# %% processing qualitive columns
+ohe = OneHotEncoder()
+ohe.fit(df[['ecoregion']])
+X_qual = ohe.transform(df[['ecoregion']])
+onehot = pd.DataFrame(X_qual.todense())
+onehot.columns = [f'eco_{i}' for i in onehot.columns]
+df = pd.concat([df, onehot], axis=1)
+
+# %%
+X = df[quantitative_col+onehot.columns.tolist()]
+y = df.labels
+X_train = X[df.subset=='train']
+y_train = y[df.subset=='train']
+X_val = X[df.subset=='val']
+y_val = y[df.subset=='val']
+# %% test random forest
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+rf.score(X_val, y_val)
+# %%
