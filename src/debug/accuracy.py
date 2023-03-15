76053abe9d20915e9601b298ba996d707c9965df
Maximilien Servajean <maximiliense@gmail.com>
2023-03-15 10:00:00 +0100

    debug

--- src/debug/accuracy.py
+++ src/debug/accuracy.py
@@ -4,13 +4,21 @@ import torch
 from torchmetrics import Accuracy
 from torchmetrics.functional import accuracy
 # %% generate predictions
-
-y = torch.randint(0, 5, size=(100,))
-y_pred = torch.rand(size=(100, 5))
+y = torch.Tensor([3, 1]).long()
+y_pred = torch.Tensor(
+    [[0.6891, 0.7101, 0.4097, 0.9859, 0.6466],
+    [0.2039, 0.6032, 0.1558, 0.7958, 0.8186]]
+)
 # %% test macro average accuracy
 # accuracy counts as 0 missing classes
-acc = Accuracy(task='multiclass', top_k=2,average='macro', num_classes=5)
+acc = Accuracy(task='multiclass', top_k=2, average='macro', num_classes=y_pred.size(1))
 print(f'Accuracy: {acc(y_pred, y)}')
-acc = accuracy(y_pred, y, num_classes=5, top_k=2, average='macro')
-print(f'functional.accuracy: {acc}')
+
+def acc(a, b):
+    return accuracy(a, b,
+                    num_classes=y_pred.size(1),
+                    top_k=2,
+                    average='macro')
+
+print(f'functional.accuracy: {acc(y_pred, y)}')
 # %%
