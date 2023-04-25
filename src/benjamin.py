de0d2c84b9bd91d2647be53820701a5bff3694c6
Maximilien Servajean <maximiliense@gmail.com>
2023-04-25 15:55:59 +0200

    overffiting badly

---
+++ src/benjamin.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_log_error, mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+
+def MetricChallangeIABiodiv(y_test_pred, y_test_true):
+    y_test_true_copie=y_test_true.copy()
+    y_test_pred_copie=y_test_pred.copy()
+
+    y_test_true_copie[y_test_true_copie<1]=1
+    y_test_pred_copie[y_test_pred_copie<1]=1
+
+
+    sum_S = 0
+    for site in range(y_test_true_copie.shape[0]):
+        S=sum(abs(np.log10(y_test_pred_copie[site])-np.log10(y_test_true_copie[site])))/y_test_true_copie.shape[1]
+        sum_S += S
+    mean_S = sum_S/y_test_true_copie.shape[0]
+    return mean_S    
+
+
+# Chemin 
+path_dir = "../data"
+data = "Galaxy117-Sort_on_data_82_log(n+1)_vec_all"
+
+path_dir_2 = "../data/"
+data_2 = "2023-04-21_16-04-56"
+
+path_dir_3 = "../data/"
+data_3 = "Galaxy117-Sort_on_data_82_n_vec_value"
+
+# Nombre de décimal pour le résultat
+score_decimal=10
+
+# type d'inputs pour le RF 
+inputs ='env+spatial'  # 'env+spatial+cnn_output' 'env+spatial' 'cnn_output'
+
+
+
+# récupération des données
+# pour le type de vecteur
+df = pd.read_csv(path_dir + "/" + data + ".csv", sep=',', index_col='SurveyID')
+# pour les sorties du CNN
+df_2 = pd.read_csv(path_dir_2 + "/" + data_2 + ".csv", sep=',', index_col='SurveyID')
+# pour les variables env et spatiales
+df_3 = pd.read_csv(path_dir_3 + "/" + data_3 + ".csv", sep=',', index_col='SurveyID')
+
+df_3bis = df_3[['SiteLat',
+            'SiteLong',
+            'TCI_sentinel_band_0_sd',
+            'TCI_sentinel_band_0_central_value',
+            'TCI_sentinel_band_1_sd',
+            'TCI_sentinel_band_1_central_value',
+            'TCI_sentinel_band_2_sd',
+            'TCI_sentinel_band_2_central_value',
+            'bathymetry_band_0_sd',
+            'bathymetry_band_0_central_value',
+            'bathy_95m_band_0_sd',
+            'bathy_95m_band_0_mean_15x15',
+            'chlorophyll_concentration_1km_band_0_sd',
+            'chlorophyll_concentration_1km_band_0_mean_30x30',
+            'east_water_velocity_4_2km_mean_day_lite_band_0_sd',
+            'east_water_velocity_4_2km_mean_day_lite_band_0_mean_7x7',
+            'east_water_velocity_4_2km_mean_day_lite_band_1_sd',
+            'east_water_velocity_4_2km_mean_day_lite_band_1_mean_7x7',
+            'east_water_velocity_4_2km_mean_day_lite_band_2_sd',
+            'east_water_velocity_4_2km_mean_day_lite_band_2_mean_15x15',
+            'east_water_velocity_4_2km_mean_month_lite_band_0_sd',
+            'east_water_velocity_4_2km_mean_month_lite_band_0_mean_7x7',
+            'east_water_velocity_4_2km_mean_month_lite_band_1_sd',
+            'east_water_velocity_4_2km_mean_month_lite_band_1_mean_7x7',
+            'east_water_velocity_4_2km_mean_month_lite_band_2_sd',
+            'east_water_velocity_4_2km_mean_month_lite_band_2_mean_15x15',
+            'meditereanean_sst_band_0_sd',
+            'meditereanean_sst_band_0_mean_15x15',
+            'north_water_velocity_4_2km_mean_day_lite_band_0_sd',
+            'north_water_velocity_4_2km_mean_day_lite_band_0_mean_7x7',
+            'north_water_velocity_4_2km_mean_day_lite_band_1_sd',
+            'north_water_velocity_4_2km_mean_day_lite_band_1_mean_7x7',
+            'north_water_velocity_4_2km_mean_day_lite_band_2_sd',
+            'north_water_velocity_4_2km_mean_day_lite_band_2_mean_15x15',
+            'north_water_velocity_4_2km_mean_month_lite_band_0_sd',
+            'north_water_velocity_4_2km_mean_month_lite_band_0_mean_7x7',
+            'north_water_velocity_4_2km_mean_month_lite_band_1_sd',
+            'north_water_velocity_4_2km_mean_month_lite_band_1_mean_7x7',
+            'north_water_velocity_4_2km_mean_month_lite_band_2_sd',
+            'north_water_velocity_4_2km_mean_month_lite_band_2_mean_15x15',
+            'salinity_4_2km_mean_day_lite_band_0_sd',
+            'salinity_4_2km_mean_day_lite_band_0_mean_7x7',
+            'salinity_4_2km_mean_day_lite_band_1_sd',
+            'salinity_4_2km_mean_day_lite_band_1_mean_7x7',
+            'salinity_4_2km_mean_day_lite_band_2_sd',
+            'salinity_4_2km_mean_day_lite_band_2_mean_15x15',
+            'salinity_4_2km_mean_month_lite_band_0_sd',
+            'salinity_4_2km_mean_month_lite_band_0_mean_7x7',
+            'salinity_4_2km_mean_month_lite_band_1_sd',
+            'salinity_4_2km_mean_month_lite_band_1_mean_7x7',
+            'salinity_4_2km_mean_month_lite_band_2_sd',
+            'salinity_4_2km_mean_month_lite_band_2_mean_15x15',
+            'sea_water_potential_temperature_at_sea_floor_4_2km_mean_day_band_0_sd',
+            'sea_water_potential_temperature_at_sea_floor_4_2km_mean_day_band_0_mean_7x7',
+            'sea_water_potential_temperature_at_sea_floor_4_2km_mean_month_band_0_sd',
+            'sea_water_potential_temperature_at_sea_floor_4_2km_mean_month_band_0_mean_7x7',]]
+
+if inputs == 'env+spatial+cnn_output' :
+    df_var =  pd.merge(df_2, df_3bis, left_index=True, right_index=True)
+elif inputs == 'env+spatial' :
+    df_var =  pd.merge(df_2.subset, df_3bis, left_index=True, right_index=True)
+elif inputs == 'cnn_output' : 
+    df_var = df_2
+'''
+code pour rechercher l'emplacement des NaN :'
+nan_locations = np.where(df_var.isna()) #  le premier tableau contenant les indices des lignes et le deuxième tableau contenant les indices des colonnes où se trouvent les valeurs NaN dans le dataframe
+df_var.iloc[:, 209].name
+'''
+
+# split des données
+train_set_index = df_var.index[df_var.subset=='train']
+val_set_index = df_var.index[df_var.subset=='val']
+
+# nom de la collone du vecteur d'espèce
+name_col_sp = df.columns[-1]
+
+
+### Mise en place du train set
+n=0
+for j in train_set_index :
+    if n==0:
+        X = np.array([df_var.loc[j][1:]])        
+        y = np.array([eval(df.loc[j,name_col_sp])])
+    else :
+        X=np.append(X,
+                    np.array([df_var.loc[j][1:]]),
+                    axis=0)
+        y=np.append(y, np.array([eval(df.loc[j,name_col_sp])]), axis=0) 
+    n=n+1
+    
+### Mise en place du test set        
+n=0
+for i in val_set_index : 
+    if n==0:
+        X_test = np.array([df_var.loc[i][1:]])        
+        y_test_true = np.array([eval(df.loc[i,name_col_sp])])
+    else :
+        X_test = np.append(X_test ,
+                                np.array([df_var.loc[i][1:]]),
+                                axis=0)
+        y_test_true = np.append(y_test_true , np.array([eval(df.loc[i,name_col_sp])]), axis=0) 
+    n=n+1
+    
+### Configuration du l'entrainement
+regr = RandomForestRegressor(max_depth=2, random_state=0, criterion="squared_error") 
+
+### Entrainement
+regr.fit(X, y)
+
+### Résultats
+y_test_pred=regr.predict(X_test)
+
+
+MAE = round(np.mean(mean_absolute_error(y_test_true, y_test_pred,multioutput="raw_values")),score_decimal)
+print(f'mean_absolute_error: {MAE}')
+
+MSLE = round(np.mean(mean_squared_log_error(y_test_true, y_test_pred,multioutput="raw_values")),score_decimal)
+print(f'mean_squared_log_error: {MSLE}')
+
+MSE = round(np.mean(mean_squared_error(y_test_true, y_test_pred,multioutput="raw_values")),score_decimal)
+print(f'mean_squared_error: {MSE}')
+
+metric_challange_ia_biodiv = round(MetricChallangeIABiodiv(np.exp(y_test_pred)-1, np.exp(y_test_true)-1),score_decimal)
+print(f'metric_challange_ia_biodiv: {metric_challange_ia_biodiv}')
+
+
+R2_sites = round(r2_score(np.transpose(y_test_true), np.transpose(y_test_pred),multioutput="uniform_average"),score_decimal)
+print(f'R2_sites: {R2_sites}')
+
+#R2_sites_log = round(r2_score(np.transpose(np.log(y_test_true+1)), np.transpose(np.log(y_test_pred+1)),multioutput="uniform_average"),3)
+#print(f'R2_sites_of_log(n+1): {R2_sites_log}')
+
+
+    
+
+X.shape
+X_test.shape
+
--- src/overfit.py
+++ src/overfit.py
@@ -7,24 +7,20 @@ import pandas as pd
 # import itertools
 
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
 # from sklearn.multioutput import MultiOutputRegressor
-# %%
-df = pd.read_csv('../data/Galaxy117-Sort_on_data_82_n_vec_value.csv')
-# %%
-ast.literal_eval(df.species_abundance_vec.iloc[0])
-# %%
-def metric_challenge(y, y_hat, n_dim=True):
-    y = y.copy()
-    y_hat = y_hat.copy()
-    y_hat[y_hat < 0] = 0
-    y[y < 0] = 0
-    if n_dim:
-        S = np.abs(y_hat-y).mean(axis=1)
-        return S.mean()
-    else:
-        return np.abs(y_hat-y)
+
 
 # %%
+df_ab = pd.read_csv('../data/Galaxy117-Sort_on_data_82_n_vec_all.csv')
+df_features = pd.read_csv('../data/Galaxy117-Sort_on_data_82_n_vec_value.csv') \
+                .drop('species_abundance_vec', axis=1)
+
+df = df_ab[['SurveyID', 'species_abundance_vec']].set_index('SurveyID') \
+                                                 .merge(df_features, how='left', on='SurveyID')
+
+# %% constructing abondance vector
+
 df_test = df[df.subset == 'val']
 y_test = [ast.literal_eval(r[1]['species_abundance_vec']) for r in df_test.iterrows()]
 y_test = np.array(y_test).astype(float)
@@ -33,24 +29,50 @@ df_train = df[df.subset == 'train']
 y_train = [ast.literal_eval(r[1]['species_abundance_vec']) for r in df_train.iterrows()]
 y_train = np.array(y_train).astype(float)
 print(y_train.shape)
-# %% testing that the metric gives 0
-y_test_log = y_test.copy()
-y_test_log[y_test_log == 0] = -float('inf')
-y_test_log[y_test_log != -float('inf')] = np.log(y_test_log[y_test_log != -float('inf')])
-y_test_log[y_test_log < -1] = -1
-print(metric_challenge(y_test_log, y_test_log))
 
+# %% consider "log abondance"
+def to_log(abondance):
+    abondance = abondance.copy()
+    abondance[abondance == 0] = -float('inf')
+    abondance[abondance != -float('inf')] = np.log(abondance[abondance != -float('inf')])
+    abondance[abondance < -1] = -1
+    return abondance
 
-y_train_log = y_train.copy()
-y_train_log[y_train_log == 0] = -float('inf')
-y_train_log[y_train_log != -float('inf')] = np.log(y_train_log[y_train_log != -float('inf')])
-y_train_log[y_train_log < -1] = -1
-# %%
-feature_names = df_train.columns[11:]
+y_test_log = to_log(y_test)
+y_train_log = to_log(y_train)
+
+# %% construct_X
+feature_names = list(df_train.columns[11:]) + ['SiteLat', 'SiteLong']
 X_train = df_train[feature_names].to_numpy()
 X_test = df_test[feature_names].to_numpy()
-# %%
+
+# %% not null
 mask = np.isnan(X_train).any(axis=0)
+sel_mask = (1-mask).astype(bool)
+print(f'Available variables: {(sel_mask).sum()}')
+print(f'Not available variables: {mask.sum()}')
+
+# %%
+list(df[feature_names].loc[:, sel_mask].columns)
+
+# %%
+def metric_challenge(y, y_hat, reduce=True):
+    """
+    This metric accepts log values...
+    """
+    y = y.copy()
+    y_hat = y_hat.copy()
+    y_hat[y_hat < 0] = 0
+    y[y < 0] = 0
+    if reduce:
+        S = np.abs(y_hat-y).mean(axis=1)
+        return S.mean()
+    else:
+        return np.abs(y_hat-y)
+
+
+# %%
+
 
 scores = []
 for sp in range(y_test_log.shape[1]):
@@ -58,19 +80,35 @@ for sp in range(y_test_log.shape[1]):
     best_score = None
     for k in range(1, 15):
         clf = KNeighborsRegressor(n_neighbors=k)
-        clf.fit(X_test[:, 1-mask], y_test_log[:, sp])
-        y_log_pred = clf.predict(X_test[:, 1-mask])
-        s = metric_challenge(y_log_pred, y_test_log[:, sp], n_dim=False)
-        print(s.mean())
+        clf.fit(X_train[:, sel_mask], y_train_log[:, sp])
+        y_log_pred = clf.predict(X_test[:, sel_mask])
+        s = metric_challenge(y_log_pred, y_test_log[:, sp], reduce=False)
+        print(f'Current score : {s.mean()}')
         if y_sel is None or best_score > s.mean():
-            print('best')
+            print('New best score')
             best_score = s.mean()
             y_sel = s
-    scores.append(s)
+    scores.append(y_sel)
     print('*' * 30)
 scores = np.stack(scores, axis=1)
-print(scores.mean(axis=0).mean())
+print(f'Total score: {scores.mean(axis=1).mean()}')
 
 # %%
-np.unique(X_test[:, 1-mask], axis=0).shape
+print(f'Number of sites: {np.unique(X_test[:, sel_mask], axis=0).shape[0]}')
+print(f'Original dimensions: {X_test.shape}')
+# %%
+clf = RandomForestRegressor(max_depth=10, random_state=0, criterion="squared_error") 
+clf.fit(X_test[:, sel_mask], y_test_log)
+y_pred = clf.predict(X_test[:, sel_mask])
+s = metric_challenge(y_pred, y_test_log)
+print(f'Random forest score test on test: {s}')
+
+# %% select one site at two moments
+df_temp = df[df.SurveyID.isin([912354162, 912354174])]
+# %%
+clf = RandomForestRegressor(max_depth=2, random_state=0, criterion="squared_error") 
+clf.fit(X_train[:, sel_mask], y_train_log)
+y_pred = clf.predict(X_test[:, sel_mask])
+s = metric_challenge(y_pred, y_test_log)
+print(f'Random forest score train on test: {s}')
 # %%
